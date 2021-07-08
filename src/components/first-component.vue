a2c91f9c1fb2975ead5ff4a906dd69bb24034400
imron <karombekh@gmail.com>
2021-07-08 23:31:34 +0500

    New project 1

---
+++ src/components/first-component.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="robotFace">
+        {{message}}.Nice to meet you, Vue.
+    </div>
+</template>
+
+<script>
+export default {
+    el: "#robotFace",
+    data() {
+        message: "Hello Vue"
+    }
+}
+</script>
